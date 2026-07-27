2228392d5035422ae989d66b13bb3a101135e412
david-pi3141 <dpinzonh@unal.edu.co>
2026-07-27 18:48:50 -0500

    [Update] Programas: Pendiente comentarlos

---
+++ Anexos/train_det.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import torch
+
+torch.cuda.empty_cache()
+
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+
+def main():
+
+    # Modelo preentrenado de detección
+    #model = YOLO(r"C:\Users\david\Desktop\ROBOTICA\Proyecto\YOLO\runs\detect\runs\Detector_Componentes-7\weights\best.pt")
+    model = YOLO(r"C:\Users\david\Desktop\ROBOTICA\Proyecto\YOLO\runs\detect\runs\Detector_Componentes_Nuevo\weights\best.pt")
+    # También puedes usar:
+    # yolo26n.pt
+    # yolo26s.pt
+    # yolo26m.pt
+    # yolo26l.pt
+
+    model.train(
+
+        data="Dataset_prefinal/data.yaml",
+        epochs=50,
+        imgsz=640,
+        batch=8,
+        device=0,
+        workers=0,
+        amp=True,
+        pretrained=True,
+        cache=False,
+        project="runs",
+        name="Detector_prefinal"
+    )
+
+if __name__ == "__main__":
+    main()
